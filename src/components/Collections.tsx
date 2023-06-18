e8b03026501e5cab349d3f550b0c7829fc7a0509
StevenSon12 <stivanstf55@gmail.com>
2023-06-18 23:59:11 +0100

    Started working on the collections component

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -1,7 +1,26 @@
 import '../styles/Collections.css'
 import { Navbar } from './Home'
+import {useEffect} from "react";
+import { getStorage, ref, listAll } from 'firebase/storage';
 
 export default function Collections(){
+
+    const storage = getStorage();
+    const listRef = ref(storage, '/random/uid');
+
+    useEffect(() => {
+        listAll(listRef)
+        .then((res) => {
+            res.prefixes.forEach((folderRef) => {
+                console.log(folderRef.bucket);
+            });
+            res.items.forEach((itemRef) => {
+                console.log(itemRef.bucket);
+            });
+        });
+    });
+
+
     return(
         <div className='collectionsWrap'>
             <Navbar />
