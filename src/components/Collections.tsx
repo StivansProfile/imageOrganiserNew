32233c9afbcdd4a59c2d9d83b59d855b142f8874
StevenSon12 <stivanstf55@gmail.com>
2023-06-19 02:06:12 +0100

    We can successfully retrieve images and folder names from storage

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -1,30 +1,51 @@
 import '../styles/Collections.css'
 import { Navbar } from './Home'
-import {useEffect} from "react";
-import { getStorage, ref, listAll } from 'firebase/storage';
+import {useEffect, useState} from "react";
+import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 
 export default function Collections(){
 
     const storage = getStorage();
-    const listRef = ref(storage, '/random/uid');
+    const listRef = ref(storage, '/random');
+
+    const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+    function addImageUrls(newElement: string){
+        setImageUrls((prevArr) => [...prevArr, newElement]);
+    }
 
     useEffect(() => {
         listAll(listRef)
         .then((res) => {
             res.prefixes.forEach((folderRef) => {
-                console.log(folderRef.bucket);
+            // console.log(folderRef);
             });
             res.items.forEach((itemRef) => {
-                console.log(itemRef.bucket);
+                // retrieves name of the folder 
+                console.log(itemRef.parent?.fullPath);
+
+                // retrieves the image url
+                getDownloadURL(itemRef)
+                .then(function(url){
+                    addImageUrls(url);
+                    console.log(url);
+                })
             });
         });
-    });
+    }, []);
 
 
     return(
         <div className='collectionsWrap'>
             <Navbar />
             <h1>Here are your collections.</h1>
+
+            {/* map out the images */}
+            {imageUrls.map((element, index) => (
+                <img key={index} src={element}
+                width="50%"></img>
+            ))}
+
         </div>
     )
 }
