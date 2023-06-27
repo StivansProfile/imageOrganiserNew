4ecb086072075d9745960f39b686fe7470d52022
StevenSon12 <stivanstf55@gmail.com>
2023-06-27 22:29:44 +0100

    Mapping out imgs using string arrays

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -3,15 +3,33 @@ import { Navbar } from './Home'
 import {useEffect, useState} from "react";
 import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 
+
+/*
+How to map out the image folders:
+Save the given folder name and image urls
+within an object
+e.g:
+{
+    folderName: "random",
+    images: [url1, url2, url3]
+}
+then map out all objects
+*/
+
 export default function Collections(){
 
     const storage = getStorage();
     const listRef = ref(storage, '/random');
 
     const [imageUrls, setImageUrls] = useState<string[]>([]);
+    const [imageFolders, setImageFolders] = useState<string[]>([]);
 
     function addImageUrls(newElement: string){
         setImageUrls((prevArr) => [...prevArr, newElement]);
+    }
+
+    function addImageFolders(newElement: string){
+        setImageFolders((prevArr) => [...prevArr, newElement]);
     }
 
     useEffect(() => {
@@ -23,6 +41,8 @@ export default function Collections(){
             res.items.forEach((itemRef) => {
                 // retrieves name of the folder 
                 console.log(itemRef.parent?.fullPath);
+                if(itemRef.parent?.fullPath)
+                addImageFolders(itemRef.parent?.fullPath);
 
                 // retrieves the image url
                 getDownloadURL(itemRef)
